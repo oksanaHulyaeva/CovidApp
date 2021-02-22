303ec645c201936c694b8141d83b383703636d15
oksanaHulyaeva <hulyaevaoksana@gmail.com>
2021-02-23 01:22:04 +0300

    feat: add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import React from 'react';
+
+import Navbar from './components/Navbar'
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../sass/navbar.scss';
+
+// type NavbarProps = {
+//   data: IData[],
+//   level: number
+// }
+
+const Navbar: React.FC = () => {
+  return (
+    <nav>sdfsf
+    </nav>
+  )
+
+}
+
+export default Navbar
